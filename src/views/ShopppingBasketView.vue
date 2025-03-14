002d427feaab52dd1f230b56468e171a8ada6957
Gabriel Costa <gubsocosta@gmail.com>
2025-03-14 12:08:25 -0300

    feat: add message to be displayed when cart is empty

--- src/views/ShopppingBasketView.vue
+++ src/views/ShopppingBasketView.vue
@@ -1,31 +1,43 @@
 <template>
   <div class="basket">
     <div class="items">
-
-      <div class="item">
-        <div class="remove">Remove item</div>
-        <div class="photo"><img alt="" src="https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg">
-        </div>
-        <div class="description">Mens Casual Premium Slim Fit T-Shirts</div>
-        <div class="price">
+      <template  v-if="productsInBag.length">
+        <div
+            v-for="(product, index) in productsInBag"
+            :key="index"
+            class="item"
+        >
+          <div class="remove" @click="this.$store.dispatch('removeFromBag', product.id)">Remove item</div>
+          <div class="photo">
+            <img :alt="product.name"  :src="product.image">
+          </div>
+          <div class="description">{{ product.title }}</div>
+          <div class="price">
             <span class="quantity-area">
-              <button disabled="">-</button>
-              <span class="quantity">1</span>
-              <button>+</button>
+              <button :disabled="product.quantity <= 1" @click="product.quantity--">-</button>
+              <span class="quantity">{{ product.quantity }}</span>
+              <button @click="product.quantity++">+</button>
             </span>
-          <span class="amount">US$ 22.30</span>
+            <span class="amount">US$ {{ (product.price * product.quantity).toFixed(2) }}</span>
+          </div>
         </div>
-      </div>
-      <div class="grand-total"> Grand Total: US$ 22.30</div>
-
+        <div class="grand-total"> Grand Total: US$ {{  }}</div>
+      </template>
+      <template v-else>
+        <h4>No items in bag yet</h4>
+      </template>
     </div>
   </div>
 </template>
 
 <script>
 
+import {mapState} from 'vuex';
+
 export default {
   name: 'ShoppingBasket',
+  computed: mapState(['productsInBag']),
+  methods: {}
 };
 </script>
 
